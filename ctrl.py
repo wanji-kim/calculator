31c8befc0f445083b483b244c9fccebb8c4ca726
wanji-kim <uridle5@daum.net>
2025-10-17 22:29:03 +0900

    Refactor main.py to be implemented in ui.py and ctrl.py

---
+++ ctrl.py
@@ -0,0 +1,9 @@
+# UI에서 입력되는 이벤트 처리나 UI 동작 제어와 관련된 내용 포함.
+class Control:
+    def __init__(self, view):
+        self.view = view
+        self.connectSignals()
+
+    def connectSignals(self):
+        self.view.btn1.clicked.connect(self.view.activateMessage)
+        self.view.btn2.clicked.connect(self.view.clearMessage)
