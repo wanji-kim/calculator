4c01241851033d61ff4098a5538e6b99c62be523
wanji-kim <uridle5@daum.net>
2025-10-17 16:56:52 +0900

    Add icon.jfif and push button with handler function in main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,
+                             QMessageBox)
+from PyQt5.QtGui import QIcon
 
 
 class Calculator(QWidget):
@@ -8,9 +10,23 @@ class Calculator(QWidget):
         self.initUI()
 
     def initUI(self):
+        self.btn1 = QPushButton('Message', self)
+        self.btn1.clicked.connect(self.activateMessage)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addWidget(self.btn1)
+        vbox.addStretch(1)
+
+        self.setLayout(vbox)
+
         self.setWindowTitle("Calculator")
+        self.setWindowIcon(QIcon('icon.jfif'))
         self.resize(256, 256)
         self.show()
+
+    def activateMessage(self):
+        QMessageBox.information(self, "information", "Button Clicked")
 
 
 if __name__ == '__main__':
