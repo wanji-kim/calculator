99fb11aea062ae20c74bb6ca7be9a539a016ba69
wanji-kim <uridle5@daum.net>
2025-10-23 18:15:01 +0900

    Modify div function using exception

--- ctrl.py
+++ ctrl.py
@@ -29,10 +29,13 @@ class Control:
         return a*b
 
     def div(self, a, b):
-        if (b==0):
-            return 0
-        else:
-            return a/b
+        try:
+
+            if (b==0):
+                raise Exception("Division Error")
+        except Exception as e:
+            return e
+        return a/b
     
     def pow(self, a, b):
         return pow(a, b)
